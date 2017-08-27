c49b8a3374287110d0d4715d03a94b33e2720480
George Alegre <facu.alegre@gmail.com>
2017-08-27 14:57:33 -0300

    Better file structure. Localization started.

---
+++ UNCmorfi/Controllers/MapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MapViewController.swift
+//  UNCmorfi
+//
+//  Created by George Alegre on 6/29/17.
+//  Copyright © 2017 George Alegre. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    let viewID = "viewID"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        mapView.delegate = self
+        
+        let uni = Comedor(title: "Ciudad Universitaria", subtitle: "Comedor en la ciudad universitaria.", coordinate: CLLocationCoordinate2D(latitude: -31.439734, longitude: -64.189293))
+        let downtown = Comedor(title: "Centro", subtitle: "Comedor universitario en el centro.", coordinate: CLLocationCoordinate2D(latitude: -31.416686, longitude: -64.189000))
+        let annotations = [uni, downtown]
+        
+        mapView.addAnnotations(annotations)
+        mapView.showAnnotations(annotations, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Comedor else { return nil }
+        
+        let annotationView: MKPinAnnotationView
+        if let view = mapView.dequeueReusableAnnotationView(withIdentifier: viewID) as? MKPinAnnotationView {
+            annotationView = view
+        } else {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: viewID)
+            annotationView.canShowCallout = true
+            annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        
+        return annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        MKMapItem(placemark: MKPlacemark(coordinate: view.annotation!.coordinate)).openInMaps(launchOptions: nil)
+    }
+}
