6b2fc08a1563f5acd2747677c096fefd47fea170
George Alegre <facu.alegre@gmail.com>
2017-11-15 00:03:27 -0300

    #12 - New user data endpoint.

--- UNCmorfi/Models/User.swift
+++ UNCmorfi/Models/User.swift
@@ -18,6 +18,7 @@ class User: Codable {
     var balance: Int
     var image: UIImage?
     var imageCode: String
+    var expiryDate: Date
 
     init(fromCode code: String) {
         self.code = code.uppercased()
@@ -25,52 +26,9 @@ class User: Codable {
         self.balance = 0
         self.imageCode = ""
         self.image = nil
+        self.expiryDate = Date()
     }
     
-    // MARK: Methods
-    func update(callback: (() -> ())? = nil) {
-        UNCComedor.getUserStatus(from: code) { error, name, balance, imageCode in
-            guard error == nil else {
-                if let callback = callback {
-                    callback()
-                }
-                return
-            }
-            
-            if let name = name, let balance = balance, let imageCode = imageCode {
-                self.name = name
-                self.balance = balance
-                self.imageCode = imageCode
-                
-                if self.image == nil {
-                    UNCComedor.getUserImage(from: imageCode) { error, image in
-                        guard error == nil else {
-                            callback?()
-                            return
-                        }
-                        
-                        self.image = image
-
-                        if let callback = callback {
-                            callback()
-                        }
-                        return
-                    }
-                } else {
-                    if let callback = callback {
-                        callback()
-                    }
-                    return
-                }
-            } else {
-                if let callback = callback {
-                    callback()
-                }
-                return
-            }
-        }
-    }
-
     // MARK: Codable
     private static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
     static let ArchiveURL = DocumentsDirectory.appendingPathComponent("users")
@@ -80,6 +38,7 @@ class User: Codable {
         case balance
         case code
         case imageCode
+        case expiryDate
 
         // Image is gonna be encoded as data and an image will be obtained from this data when decoding.
         case image
@@ -91,6 +50,7 @@ class User: Codable {
         balance = try values.decode(Int.self, forKey: .balance)
         code = try values.decode(String.self, forKey: .code)
         imageCode = try values.decode(String.self, forKey: .imageCode)
+        expiryDate = try values.decode(Date.self, forKey: .expiryDate)
 
         if let imageBase64 = try values.decodeIfPresent(String.self, forKey: .image),
             let imageData = Data(base64Encoded: imageBase64) {
@@ -106,6 +66,7 @@ class User: Codable {
         try container.encode(balance, forKey: .balance)
         try container.encode(code, forKey: .code)
         try container.encode(imageCode, forKey: .imageCode)
+        try container.encode(expiryDate, forKey: .expiryDate)
 
         if let image = image,
             let imageData = UIImagePNGRepresentation(image) {
@@ -117,5 +78,87 @@ class User: Codable {
 extension User: Equatable {
     static func ==(lhs: User, rhs: User) -> Bool {
         return lhs.code == rhs.code
+    }
+}
+
+extension Array where Element == User {
+    func matchingOrder(of users: [Element]) throws -> [Element] {
+        let result: [Element]
+        
+        result = users.flatMap { (user) -> Element? in
+            guard let index = self.index(of: user) else {
+                return nil
+            }
+            
+            return self[index]
+        }
+        
+        if result.count != self.count {
+            throw NSError()
+        }
+        
+        return result
+    }
+    
+    /**
+     Updates a collection of users.
+     - callback: handler to be called with updated users.
+     */
+    func update(callback: @escaping (([User]) -> ())) {
+        // Create a queue for parallel jobs.
+        let queue = DispatchQueue.global(qos: .userInitiated)
+        let group = DispatchGroup()
+
+        // Return the original data if the update fails.
+        let backup: [User] = self
+        var updateFailed = false
+        
+        var result: [User] = []
+        var images: [String: UIImage] = [:]
+
+        // Get the user codes needed for the user API.
+        let userCodes = map { $0.code }
+        
+        // Get the updated users from the API.
+        group.enter()
+        UNCComedor.getUsers(from: userCodes) { (error, users) in
+            // Notify that this task is done.
+            defer { group.leave() }
+            
+            guard error == nil, let users = users else {
+                updateFailed = true
+                return
+            }
+            
+            result = users
+            
+            // Get the updated user images from the API.
+            users.forEach { (user) in
+                group.enter()
+                UNCComedor.getUserImage(from: user.imageCode) { (error, image: UIImage?) in
+                    // Notify that this task is done.
+                    defer { group.leave() }
+                    
+                    guard error == nil else {
+                        updateFailed = true
+                        return
+                    }
+                    
+                    // Maybe the user doesn't have an image.
+                    if let image = image {
+                        user.image = image
+                    }
+                }
+            }
+        }
+        
+        // When all tasks have finished.
+        group.notify(queue: queue) {
+            if updateFailed {
+                callback(backup)
+            } else {
+                callback((try? result.matchingOrder(of: self)) ?? backup)
+            }
+        }
     }
 }
